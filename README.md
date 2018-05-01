f7bc8155fa480ec2f02342bf0dde907711d4006b
Simon Sadedin <ssadedin@gmail.com>
2018-05-01 12:26:22 +1000

    added initial code base, extracted from gngs

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-# bazam
+# Bazam
+
 A read extraction and realignment tool for next generation sequencing data
+
+## Why?
+
+Most sequencing data is stored in coordinate sorted BAM files, because that's
+how most analyses want to use it. However, when reads are paired, seeing both
+reads of the pair at the same time also important for some applications. For
+example, if you want to realign the reads to a different genome reference, or
+do other actions such as trimming them based on overlap, etc., then you need
+this.  Doing this when the reads are stored in coordinate order is
+unfortunately not easy. Most approaches involve leaving out some of the
+reads,  require re-sorting the BAM file (requiring substantial time and space) or
+are unacceptable slow. Bazam is a tool that efficiently extracts read pairs from 
+BAM files that are stored in coordinate sorted order. In particular, Bazam
+makes it especially easy to realign reads to a new genome without any need for 
+temporary intermediate files.
+
+## Why is this hard?
+
+You might think this problem should be easy. However it's difficult because
+when stored in coordinate order the only efficient way to read the file is 
+in coordinate order. Yet a read's mate may be stored at a significant
+coordinate distance, meaning that to see both a read and it's mate 
+you either need to do an expensive random lookup of each mate
+(too slow), or buffer reads in memory until their mate becomes
+available in the stream (too memory expensive). 
+
+## How Does Bazam Solve this?
+
+Bazam doesn't do any magic: it just carefully buffers reads using various different 
+strategies for caching to try and minimise memory use. It is also careful designed
+to run fast so that it's probably faster than any downstream application (such as 
+realignment) that you are trying to do on the data.
+
+
+## Getting it
+
+Just clone it and build with Gradle:
+
+```
+git clone git@github.com:ssadedin/bazam.git
+cd bazam
+gradle clean jar
+```
+
+## Running it
+
+To get the help, you just execute the JAR file:
+
+```
+java -jar build/libs/bazam.jar
+```
+
+## Simple Example
+
+Let's get all the read pairs from a BAM File in interleaved FASTQ format:
+
+```
+java -jar build/libs/bazam.jar -bam  test.bam  > tmp.fastq
+```
+
+## Using Filtering
+
+You can add a filter to select which reads you want extracted. The filter is a groovy expression that is executed with 
+a variable called "pair". The "pair" variable has attributes `r1` and `r2` which are [SAMRecord](https://samtools.github.io/htsjdk/javadoc/htsjdk/htsjdk/samtools/SAMRecord.html) objects, enabling you to access all the attributes of the reads. If the expression returns `true` then the read is emitted, otherwise it is dropped:
+
+Get only the reads where the pair spans different chromosomes:
+
+```
+java -jar build/libs/bazam.jar -f "pair.r1.referenceIndex != pair.r2.referenceIndex" -bam  test.bam > chimeric_reads.fastq
+```
+
+## Other Options
+
+For greater performance, you can shard reads using `-s` which will cause only every n out of N reads to be 
+emitted (specified in the form `-s n,N`).
+
+You can supply a BED file to restrict regions that reads are harvested from with `-L` (note: both reads
+of a pair are emitted if either one overlaps the regions at all). 
+
+Use the `-namepos` option to cause reads to be emitted with modified read names that embed their original
+positions in the source BAM file. This is useful if you want to track where reads came from after downstream
+processing (for example, comparing two aligners, etc).
+
+## Advanced Example
+
+You can see an advanced example of Bazam being used in the STRetch pipeline:
+
+https://github.com/Oshlack/STRetch/blob/feat-faster-bam-pipeline/pipelines/pipeline_stages.groovy#L101
+
+Note that in this example it is invoked using a pure `java` command.
+
---
+++ src/main/groovy/bazam/Bazam.groovy
@@ -0,0 +1,127 @@
+/*
+ *  Groovy NGS Utils - Some simple utilites for processing Next Generation Sequencing data.
+ *
+ *  Copyright (C) 2018 Simon Sadedin, ssadedin<at>gmail.com
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package bazam
+
+import gngs.*
+import gngs.pair.PairLocator
+import gngs.pair.PairScanner
+import groovy.util.logging.Log
+import groovyx.gpars.actor.DefaultActor
+
+/**
+ * Extracts read pairs from BAM or CRAM files sorted in coordinate order.
+ * <p>
+ * Extracting paired reads from BAM files when sorted in coordinate order is 
+ * problematic because each read's mate may be stored at a distance from the read
+ * itself within the BAM file. Random lookup of each mate is too slow, so it is
+ * necessary to buffer reads until their mate becomes available through efficient
+ * linear scan. This in turn however leads to significant memory usage, requiring
+ * careful design to balance performance and memory.
+ * 
+ * @author Simon Sadedin
+ */
+@Log
+class Bazam extends ToolBase {
+    
+    SAM bam
+
+    @Override
+    public void run() {
+        log.info "Extracting read pairs from $opts.bam"
+        bam = new SAM(opts.bam)
+        
+        Writer out
+        if(opts.o) {
+            out = new BufferedWriter(new File(opts.o).newWriter(), 2024*1024)
+        }
+        else {
+            out = System.out.newWriter()
+        }
+        
+        out.withWriter { 
+            PairScanner scanner = new PairScanner(out, opts.n ? opts.n.toInteger():4, opts.L?getRegions():null, opts.f?:null)
+            if(opts.s) {
+                if(!opts.s ==~ /[0-9]*,[0-9*]/)
+                    throw new IllegalArgumentException("Please provide shard number and total number of shards in form s,N to -s")
+                    
+                scanner.shardId = opts.s.tokenize(',')[0].toInteger()-1
+                if(scanner.shardId<0)
+                    throw new IllegalArgumentException("Please specify shard id > 0")
+                    
+                scanner.shardSize = opts.s.tokenize(',')[1].toInteger()
+                if(scanner.shardId >= scanner.shardSize)
+                    throw new IllegalArgumentException("Please specify shard id < number of shards ($scanner.shardSize)")
+            }
+            
+            if(opts.namepos)
+                scanner.formatter.addPosition = true
+                
+            scanner.scan(bam)
+            
+            // Debug option: dumps residual unpaired reads at end
+            if(opts.du) {
+                scanner.locators.each { PairLocator locator ->
+                    if(!locator.buffer.isEmpty()) {
+                        log.info "ERROR: Residual reads in locator: "
+                        locator.buffer.each { key, value ->
+                            log.info "$key: $value.r1ReferenceName:$value.r1AlignmentStart"
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    Regions getRegions() {
+        log.info "Initialising regions to scan from $opts.L"
+        Regions regions
+        if(opts.L.endsWith('.bed')) {
+            regions = new BED(opts.L).load()
+        }
+        else {
+            regions = new Regions()
+            regions.addRegion(new Region(opts.L))
+        }
+        
+        log.info "There are ${Utils.humanBp(regions.size())} included regions"
+        
+        if(opts.pad) {
+            log.info "Padding regions by ${opts.pad}bp"
+            regions = regions.widen(opts.pad.toInteger())
+            regions = regions.reduce()
+            log.info "After padding, regions span ${regions.size()}bp (${Utils.humanBp(regions.size())})"
+        }
+  
+        return regions
+    }
+    
+    static void main(args) {
+        cli('Bazam -bam <bam> -L <regions>', args) {
+            bam 'BAM file to extract read pairs from', args:1, required: true
+            pad 'Amount to pad regions by (0)', args:1, required: false
+            n 'Concurrency parameter (4)', args:1, required: false
+            s 'Sharding factor: format <n>,<N>: output only reads belonging to shard n of N', args:1, required: false
+            namepos 'Add original position to the read names', required:false
+            'L' 'Regions to include reads (and mates of reads) from', longOpt: 'regions', args:1, required: false
+            'f' 'Filter using specified groovy expression', longOpt: 'filter', args:1, required: false
+            o 'Output file', args:1, required: false
+        }
+    }
+}
