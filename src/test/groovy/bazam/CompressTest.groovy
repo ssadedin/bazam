17fb4724cb01b91d7a88f2ecb644ca8157c3cc91
Simon Sadedin <ssadedin@gmail.com>
2019-05-01 21:38:21 +1000

    fix tests failing on linux due to snappy library misidentified

--- src/test/groovy/bazam/CompressTest.groovy
+++ src/test/groovy/bazam/CompressTest.groovy
@@ -7,11 +7,15 @@ import gngs.*
 class CompressTest {
     static {
        // Needed to initialize the snappy library, if that is used to compress reads in memory
-       System.setProperty("org.xerial.snappy.lib.name", "libsnappyjava.jnilib");
+        if(System.getProperty("os.name").toLowerCase().contains("Mac")) {
+           System.setProperty("org.xerial.snappy.lib.name", "libsnappyjava.jnilib");
+        }
     }
 
     @Test
     public void test() {
+        
+        
         FASTQRead fastq = FASTQ.consumeRead(new File('src/test/data/test_compress.fastq').newReader())
         
         BaseCompactor compactor = new BaseCompactor()
